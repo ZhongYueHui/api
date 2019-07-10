693e031ef2420e797bd3f1e8af0fb7d323c33142
钟跃辉 <898542119@qq.com>
2019-07-10 09:43:19 +0800

    提交接口文件

---
+++ goodsList.php
@@ -0,0 +1,148 @@
+<?php 
+header('Access-Control-Allow-Origin:*');
+  $page = $_GET['page'];
+  $id = $_GET['id'];
+  $arr2 = array(
+    array("title"=>"vivo iQOO（8GB RAM/全网通）",
+    "price"=>"2988",
+    "oldprice"=>"3188",
+    "residue"=>"451",
+    "url"=>"https://2e.zol-img.com.cn/product/196_220x165/340/ceZD20aTUfn3k.jpg",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984575das",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"1"
+  ),
+    array("title"=>"苹果iPhone XS Max（全网通）",
+    "price"=>"8099",
+    "oldprice"=>"9999",
+    "residue"=>"994",
+    "url"=>"https://2a.zol-img.com.cn/product/198_220x165/766/ce70lxJGbAtfo.jpg",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984575412qwe",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"2"
+  ),
+    array("title"=>"vivo Z5x（4GB/64GB/全网通）",
+    "price"=>"1388",
+    "oldprice"=>"1599",
+    "residue"=>"266",
+    "url"=>"https://2b.zol-img.com.cn/product/198_220x165/991/ceWAPr91KoN8Y.jpg",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"01998457541ad45z",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"3"
+  ),
+    array("title"=>"华为 P30 Pro",
+    "price"=>"5488",
+    "oldprice"=>"5688",
+    "residue"=>"1000",
+    "url"=>"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3221254683,1086050666&fm=179&app=42&f=JPEG?w=121&h=91",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984575dsa5454zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"4"
+  ),
+    array("title"=>"OPPO Reno 10倍变焦版",
+    "price"=>"4799",
+    "oldprice"=>"4999",
+    "residue"=>"998",
+    "url"=>"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=4158203526,1023063201&fm=179&app=42&f=JPEG?w=121&h=91",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984578487das125zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"5"
+  ),
+    array("title"=>"vivo X27",
+    "price"=>"3598",
+    "oldprice"=>"3899",
+    "residue"=>"156",
+    "url"=>"https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=581834081,1846284202&fm=179&app=42&f=JPEG?w=121&h=91", 
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984qwe454125zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"6"
+  ),
+    array("title"=>"三星 Galaxy S10 5G版",
+    "price"=>"12999",
+    "oldprice"=>"14999",
+    "residue"=>"998",
+    "url"=>"https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2424937687,367325688&fm=179&app=42&f=JPEG?w=121&h=91", "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"0199845eqwe44125zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"7"
+  ),
+    array("title"=>"一加7 Plus",
+    "price"=>"3599",
+    "oldprice"=>"3999",
+    "residue"=>"125",
+    "url"=>"https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=4112935318,1918143761&fm=179&app=42&f=JPEG?w=121&h=91", "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"019984das778454125zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"8"
+  ),
+    array("title"=>"华为Mate X（5G版全网通）",
+    "price"=>"17500",
+    "oldprice"=>"17500",
+    "residue"=>"1500",
+    "url"=>"https://2b.zol-img.com.cn/product/196_220x165/599/cezEeUuDSwL5.jpg",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"01998457541fdg525zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"9"
+  ),
+    array("title"=>"OPPO K3（6GB/64GB/全网通）",
+    "price"=>"1599",
+    "oldprice"=>"1899",
+    "residue"=>"545",
+    "url"=>"https://2f.zol-img.com.cn/product/198_220x165/777/ceKElXsn3GEQE.jpg",
+    "msrc"=>"https://res.vmallres.com/pimages//product/6901443293513/428_428_1555464685019mp.png",
+    "msrcI"=>"https://res.vmallres.com/pimages//product/6901443273980/428_428_1555730194235mp.png",
+    "msrcII"=>"https://res.vmallres.com/pimages/detailImg/2019/03/25/20190325104207022934.jpg",
+    "time"=>"2019-4-5 20：15",
+    "number"=>"0199845774875zz",
+    "content"=>"定制 双 3D 曲面机身，正面几乎都是屏幕的超高屏占比。备受瞩目的莹润光泽，倍感舒适的玻璃质感，让 OnePlus 7 Pro 自带一种溢出屏幕般的精致。百般惊艳，更是百看不厌。+ 分辨率，定制 90Hz 屏幕刷新率......点开它，目之所及，就是一场视觉盛宴；滑动它，所到之处，都让指尖尽享丝滑。这，就是 90Hz 流体屏。全新 OnePlus 7 Pro，为你的期待而来，亦将全面超越你的期待。",
+    "id"=>"10"
+  ),
+  );
+  // if($id){
+  //   echo json_encode(array_slice($arr2,0,2* $id));
+  // }
+  if($page) {
+    echo json_encode(array_slice($arr2,0,6* $page));
+  } 
+  else if($id){
+    echo json_encode(array_slice($arr2,$id-1,1));
+ 
+  }else{
+  echo json_encode($arr2);
+  }
+
+?>
